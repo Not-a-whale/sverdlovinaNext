aff328c0409cf4756c09fef7a50094fd2f522407
sabao <sabaoth@ukr.net>
2024-04-26 16:47:53 +0300

    url

--- src/components/partials/Reviews/RewiewSlide.tsx
+++ src/components/partials/Reviews/RewiewSlide.tsx
@@ -1,5 +1,5 @@
 import { IReview } from "@/shared/interfaces/review.interface";
-import Image from "next/image";
+import Image, { ImageLoaderProps } from "next/image";
 
 type ReviewSlideProps = {
   review: IReview;
@@ -8,6 +8,10 @@ type ReviewSlideProps = {
 const RewiewSlide = ({ review }: ReviewSlideProps) => {
   console.log(`https://sverdlovina-next.vercel.app/api${review.imageUrl}`);
 
+  const imageLoader = ({ src, width, quality }: ImageLoaderProps) => {
+    return `${process.env.NEXT_PUBLIC_URL}/api${src}?w=${width}&q=${quality || 75}`;
+  };
+
   return (
     <div className="flex flex-col justify-between items-start mb-12 gap-12 mx-12 mt-10">
       <div className="shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] rounded-xl p-6">
@@ -15,6 +19,7 @@ const RewiewSlide = ({ review }: ReviewSlideProps) => {
       </div>
       <div className="flex gap-5 justify-between items-start">
         <Image
+          loader={imageLoader}
           src={`${process.env.NEXT_PUBLIC_URL}/api${review.imageUrl}`}
           alt="Фото профілю"
           width={50}
