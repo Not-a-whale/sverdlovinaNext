caae2f79be6e2daff3e0bf256b59d4a699434d5c
sabao <sabaoth@ukr.net>
2024-04-26 13:17:29 +0300

    logging added

--- src/components/partials/Reviews/index.tsx
+++ src/components/partials/Reviews/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import SectionHeading from "@/components/SectionHeading";
 import { IReview } from "@/shared/interfaces/review.interface";
 import ReviewSlider from "@/components/partials/Reviews/ReviewSlider";
@@ -13,6 +15,7 @@ const getData = async (): Promise<IReview[] | undefined> => {
 
 const Reviews = async () => {
   const data = await getData();
+  console.log("data", data);
 
   if (!data) return null;
   return (
