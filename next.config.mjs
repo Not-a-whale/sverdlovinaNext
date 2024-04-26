f675c99d7632fba963f6483c483ed434346eec2a
sabao <sabaoth@ukr.net>
2024-04-26 15:15:47 +0300

    hostname

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "**",
+                hostname: "sverdlovina-next.vercel.app",
             },
         ],
     },
