39a4dff53ceda4ab49762451a3e7a12bf69c5976
sabao <sabaoth@ukr.net>
2024-04-26 13:24:08 +0300

    headers added

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,19 @@ const nextConfig = {
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'uk',
+    headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+        },
+        {
+            key: 'Access-Control-Allow-Headers',
+            value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+    ],
 }
 
 const withNextIntl = createNextIntlPlugin();
