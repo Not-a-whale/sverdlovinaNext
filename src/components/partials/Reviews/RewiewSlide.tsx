5f0c75428c7b26006e20388e28cc82a2466bbe97
sabao <sabaoth@ukr.net>
2024-04-26 16:49:58 +0300

    url

--- src/components/partials/Reviews/RewiewSlide.tsx
+++ src/components/partials/Reviews/RewiewSlide.tsx
@@ -9,7 +9,7 @@ const RewiewSlide = ({ review }: ReviewSlideProps) => {
   console.log(`https://sverdlovina-next.vercel.app/api${review.imageUrl}`);
 
   const imageLoader = ({ src, width, quality }: ImageLoaderProps) => {
-    return `${process.env.NEXT_PUBLIC_URL}/api${src}?w=${width}&q=${quality || 75}`;
+    return `${src}?w=${width}&q=${quality || 75}`;
   };
 
   return (
